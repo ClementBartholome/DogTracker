9bab83aed2d7f96d51b736f48005ea5048b6ca1b
Clément Bartholomé <clementoss@gmail.com>
2025-01-27 14:26:03 +0100

    feat(DogDashboard): séparation logique métier / vue

---
+++ Components/Pages/DogDashboard.razor.cs
@@ -0,0 +1,23 @@
+﻿using DogTracker.Models;
+using DogTracker.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace DogTracker.Components.Pages
+{
+    public partial class DogDashboard
+    {
+        [Parameter] public int DogId { get; set; }
+        
+        private List<Walk> recentWalks = new();
+        private List<WeightRecord> weightHistory = new();
+        private List<Expense> expenses = new();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            recentWalks = await DogService.GetRecentWalksAsync(DogId);
+            weightHistory = await DogService.GetWeightHistoryAsync(DogId);
+            expenses = await DogService.GetExpensesAsync(DogId, DateTime.Now.AddMonths(-1), DateTime.Now);
+        }
+    }
+}
