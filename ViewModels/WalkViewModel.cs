b6d677f47fd9a8de4f974a4729beca5e9878a98a
Clément Bartholomé <clementoss@gmail.com>
2025-01-27 14:26:31 +0100

    feat(WalkTracker): ajout d'un view model pour formatter les dates

---
+++ ViewModels/WalkViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DogTracker.Models
+{
+    public class WalkViewModel
+    {
+        public Walk Walk { get; set; }
+
+        public WalkViewModel(Walk walk)
+        {
+            Walk = walk;
+        }
+
+        public DateTime AdjustedStartTime => Walk.StartTime.AddHours(1);
+        public DateTime AdjustedEndTime => Walk.EndTime.AddHours(1);
+        public double Distance => Walk.Distance;
+        public string? Route => Walk.Route;
+        public string? Notes => Walk.Notes;
+        public int DogId => Walk.DogId;
+    }
+}
