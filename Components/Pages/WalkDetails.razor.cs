fdcf9e5f45066918f08503a04e9b266e3eec3076
Clément Bartholomé <clementoss@gmail.com>
2025-02-07 17:50:35 +0100

    feat(WalkDetails): utilisation vm

---
+++ Components/Pages/WalkDetails.razor.cs
@@ -0,0 +1,20 @@
+﻿using DogTracker.Models;
+using DogTracker.Services;
+using DogTracker.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace DogTracker.Components.Pages;
+
+public partial class WalkDetails : ComponentBase
+{
+    [Parameter] public int WalkId { get; set; }
+
+    private Walk? Walk { get; set; }
+    private WalkViewModel? WalkViewModel { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Walk = await WalkService.GetWalkByIdAsync(WalkId);
+        if (Walk != null) WalkViewModel = new WalkViewModel(Walk);
+    }
+}
