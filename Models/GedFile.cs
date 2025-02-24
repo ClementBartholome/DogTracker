3b4c0924bdf351e07b433e8339ce61bba51e3e9f
Clément Bartholomé <clementoss@gmail.com>
2025-02-24 18:54:53 +0100

    feat(Ged): ajout du modèle

---
+++ Models/GedFile.cs
@@ -0,0 +1,11 @@
+﻿using DogTracker.Enums;
+
+namespace DogTracker.Models;
+
+public class GedFile
+{
+    public string PreviewUrl { get; set; }
+    public string DownloadUrl { get; set; }
+    public TypeFilesEnum Category { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
