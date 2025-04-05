d82ef8a7d6029753694b1469feeaad403d7886ad
Clément Bartholomé <clementoss@gmail.com>
2025-04-05 19:09:40 +0200

    refactor(expense): modification des types de catégorie + ajout méthode d'extension pour récup le DisplayName d'une valeur d'enum

--- Enums/ExpenseCategoryEnum.cs
+++ Enums/ExpenseCategoryEnum.cs
@@ -1,11 +1,21 @@
-﻿namespace DogTracker.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogTracker.Enums;
 
 public enum CategoryEnum
 {
+    [Display(Name = "Croquettes")]
     Croquettes,
+    [Display(Name = "Friandises")]
     Friandises,
+    [Display(Name = "Vétérinaire")]
     Vétérinaire,
+    [Display(Name = "Accessoires")]
     Accessoires,
+    [Display(Name = "Jouets")]
+    Jouets,
+    [Display(Name = "Santé")]
     Santé,
+    [Display(Name = "Autre")]
     Autre
 }
