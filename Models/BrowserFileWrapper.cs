d51f90a3d893d352aa1a3fec65e2f24825080a68
Clément Bartholomé <clementoss@gmail.com>
2025-05-18 19:38:42 +0200

    feat(blob): début d'implémentation du Azure Blob Storage pour le stockage des fichiers

---
+++ Models/BrowserFileWrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace DogTracker.Models;
+
+public class BrowserFileWrapper(Stream stream, string name) : IBrowserFile
+{
+    public string Name { get; } = name;
+    public DateTimeOffset LastModified => DateTimeOffset.Now;
+    public long Size => stream.Length;
+    public string ContentType => "application/octet-stream";
+
+    public Stream OpenReadStream(long maxAllowedSize = 512000, CancellationToken cancellationToken = default)
+    {
+        if (stream.Length > maxAllowedSize)
+        {
+            throw new IOException("Le fichier dépasse la taille maximale autorisée.");
+        }
+        return stream;
+    }
+}
